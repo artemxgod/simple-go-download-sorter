84fd2e2faf2c82545a6755a65c30154275e0344b
artem karapetyan <artemka111777@mail.ru>
2023-02-08 16:21:42 +0300

    add ext

--- sort.go
+++ sort.go
@@ -1,63 +1,68 @@
 package main
 
 import (
+	//"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if len(os.Args) == 2 {
-		godotenv.Write(map[string]string{
-			"DOWNLOAD_PATH": os.Args[1],
-		}, "./.env")
-		err := godotenv.Load()
-		check_err(err, "loading .env file")
-		if _, err = os.Stat(os.Getenv("DOWNLOAD_PATH")); err != nil {
-			godotenv.Write(map[string]string{
-				"DOWNLOAD_PATH": "d:\\downloads",
-			}, "./.env")
-			log.Fatal(err)
-		}
-	}
-	err := godotenv.Load()
-	check_err(err, "loading .env file")
-	download_path := os.Getenv("DOWNLOAD_PATH")
-	// log.Println(download_path)
-	files, err := ioutil.ReadDir(download_path)
+	files, err := ioutil.ReadDir(".")
 	check_err(err, "reading dir")
-	dirs := []string {"music", "vid", "img", "font", "drawio", "docs", "installer", "codesample"}
-	for idx := range dirs {
-		dirs[idx] = download_path + "\\" + dirs[idx]
-		check_dir(dirs[idx])
-	}
-	extmap := map[string][]string {
-		dirs[0]: 	{"mp3"},
-		dirs[1]: 	{"mp4"},
-		dirs[2]:	{"jpg", "jpeg", "png", "psd", "ico", "bmp", "svg"},
-		dirs[3]:  	{"ttf"},
-		dirs[4]:	{"drawio"},
-		dirs[5]:	{"pdf", "docx", "pptx", "pptm", "xlsx"},
-		dirs[6]:	{"msi", "exe"},
-		dirs[7]:	{"cpp", "go", "py"},
-	}
-
+	check_dir("music")
+	check_dir("vid")
+	check_dir("font")
+	check_dir("img")
+	check_dir("drawio")
+	check_dir("docs")
+	check_dir("codesample")
+	photoext := "jpgjpegpngpsdicobmpsvgheic"
+	docext := "pdfdocxpptxxlsx"
+	installerext := "msiexe"
+	fontext := "ttfwoff"
+	codeext := "cppgopy"
 	for _, file := range files {
 		if !file.IsDir() {
 			str := strings.Split(file.Name(), ".")
-			for _, dir := range dirs {
-				//log.Println(str)
-				if contains_ext(strings.ToLower(str[len(str)-1]), extmap[dir]) {
-					new_name := strings.Join([]string{dir, "\\", file.Name()}, "")
-					log.Printf("dir - %s\nfilename - %s\nextmap - %s\nnewname - %s\n",dir, str, extmap[dir], new_name)
-					err = os.Rename(download_path + "\\" + file.Name(), new_name)
-					check_err(err, "rename")
-				}
+			switch {
+			case strings.ToLower(str[len(str)-1]) == "mp3":
+				musicdir := "./music"
+				musicdir = strings.Join([]string{musicdir, "/", file.Name()}, "")
+				os.Rename(file.Name(), musicdir)
+			case strings.ToLower(str[len(str)-1]) == "mp4":
+				videodir := "./vid"
+				videodir = strings.Join([]string{videodir, "/", file.Name()}, "")
+				os.Rename(file.Name(), videodir)
+			case strings.Contains(photoext, strings.ToLower(str[len(str)-1])):
+				photodir := "./img"
+				photodir = strings.Join([]string{photodir, "/", file.Name()}, "")
+				os.Rename(file.Name(), photodir)
+			case strings.Contains(fontext, strings.ToLower(str[len(str)-1])):
+				fontdir := "./font"
+				fontdir = strings.Join([]string{fontdir, "/", file.Name()}, "")
+				os.Rename(file.Name(), fontdir)
+			case str[len(str)-1] == "drawio":
+				drawiodir := "./drawio"
+				drawiodir = strings.Join([]string{drawiodir, "/", file.Name()}, "")
+				os.Rename(file.Name(), drawiodir)
+			case strings.Contains(docext, strings.ToLower(str[len(str)-1])):
+				docdir := "./docs"
+				docdir = strings.Join([]string{docdir, "/", file.Name()}, "")
+				os.Rename(file.Name(), docdir)
+			case strings.Contains(installerext, strings.ToLower(str[len(str)-1])):
+				installerdir := "./installer"
+				installerdir = strings.Join([]string{installerdir, "/", file.Name()}, "")
+				os.Rename(file.Name(), installerdir)
+			case strings.Contains(codeext, strings.ToLower(str[len(str)-1])):
+				codedir := "./codesample"
+				codedir = strings.Join([]string{codedir, "/", file.Name()}, "")
+				os.Rename(file.Name(), codedir)
+			default:
+				log.Println(str[1])
 			}
-		} 
+		}
 	}
 }
 
@@ -67,19 +72,9 @@ func check_dir(name string) {
 		os.Mkdir(name, os.ModePerm)
 	}
 }
-
-func contains_ext(name string, extensions []string) bool {
-	for _, ext := range extensions {
-		if ext == name {
-			return true
-		}
-	}
-	return false
-}
-
 func check_err(err error, msg string) {
 	if err != nil {
 		msg := strings.ToUpper(msg)
-		log.Fatal(msg, "\t", err)
+		log.Fatal(err, msg)
 	}
 }
